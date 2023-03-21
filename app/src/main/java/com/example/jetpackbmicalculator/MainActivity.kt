88bfc1b16c32bd56ee4158212f94503a5678744e
sanao1006 <naosak1006@gmail.com>
2023-03-21 16:55:13 +0900

    put title

--- app/src/main/java/com/example/jetpackbmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/jetpackbmicalculator/MainActivity.kt
@@ -3,13 +3,19 @@ package com.example.jetpackbmicalculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.jetpackbmicalculator.ui.theme.JetpackBMICalculatorTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,6 +25,9 @@ class MainActivity : ComponentActivity() {
             JetpackBMICalculatorTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                    Column(horizontalAlignment = Alignment.Start) {
+                        Text(text = "BMI計算アプリ", fontSize = 26.sp, fontWeight = FontWeight.ExtraBold, modifier = Modifier.padding(20.dp))
+                    }
 
                 }
             }
